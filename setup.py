3de01297bf1483af1a6c93074e0e27b88124e8ba
nimmyjoy <nimmyjoykarickadu@gmail.com>
2022-12-29 07:08:12 +0530

    completed

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ HYPHEN_E_DOT = "-e ."
 def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        requirement_list = requirement_file.readlines()python data
+        requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
     if HYPHEN_E_DOT in requirement_list:
@@ -16,9 +16,8 @@ def get_requirements()->List[str]:
     return requirement_list
 
 
-
 setup(
-    name="sensor",
+    name="Sensor",
     version="0.0.2",
     author="nimmyjoy",
     author_email="nimmyjoykarickadu@gmail.com",
